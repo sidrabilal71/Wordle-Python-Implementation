13e29288af7f55879ca9ebcf88cadc5f4394973c
sidrabilal71 <sidrabilal71@gmail.com>
2025-05-18 21:51:33 +0200

    UC9: Play Again Option added in main.py

--- main.py
+++ main.py
@@ -6,16 +6,20 @@ class Main:
         self.settings = GameSettings()
 
     def run(self):
-        print("Welcome to the Wordle Enhanced Game!")                               # Welcome the user to the game
-        self.settings.configure_settings()                                          # Starting point of the game is by specifying the settings
-        print("Game will start with the following settings:")                       # Showing which settings have been established
-        print(self.settings)
+        while True:
+            print("Welcome to the Wordle Enhanced Game!")                               # Welcome the user to the game
+            self.settings.configure_settings()                                          # Starting point of the game is by specifying the settings
+            print("Game will start with the following settings:")                       # Showing which settings have been established
+            print(self.settings)
+            game_session = start_new_game({
+                "difficulty": self.settings.difficulty,
+                "length": self.settings.word_length
+            })
 
-
-        game_session=start_new_game({
-    "difficulty": self.settings.difficulty,
-    "length": self.settings.word_length
-})
+            play_again = input("\n🔁 Do you want to play again? (yes/no): ").strip().lower()
+            if play_again not in ["yes", "y"]:
+                print("👋 Thanks for playing! Goodbye.")
+                break
 
 # Example of running the main class
 if __name__ == "__main__":
